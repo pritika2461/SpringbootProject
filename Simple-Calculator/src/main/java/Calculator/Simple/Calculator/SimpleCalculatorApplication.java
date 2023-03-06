df3f23557fd1a437389396b1f64831f3f643ed1d
Pritika Somase <120477646+pritika2461@users.noreply.github.com>
2023-03-06 12:20:13 +0530

    Add files via upload

---
+++ Simple-Calculator/src/main/java/Calculator/Simple/Calculator/SimpleCalculatorApplication.java
@@ -0,0 +1,17 @@
+package Calculator.Simple.Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCalculatorApplication
+
+ {
+
+	public static void main(String[] args)
+
+      {
+		SpringApplication.run(SimpleCalculatorApplication.class, args);
+	}
+
+}
